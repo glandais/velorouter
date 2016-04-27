6b2adaf87896545fc044f155ec4351d1a9010cb3
glandais <gabriel.landais@gmail.com>
2016-04-27 10:22:53 +0200

    Fullscreen

--- js/router.map.js
+++ js/router.map.js
@@ -1,5 +1,6 @@
 var map = L.map('map', {
-    zooms: [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+    zooms: [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21],
+	fullscreenControl: true
 });
 
 if (!map.restoreView()) {
@@ -82,4 +83,6 @@ map.on('baselayerchange',function(e){
 
 map.loadGpx();
 
-L.control.locate().addTo(map);
+L.control.locate({
+    position: "topright"
+}).addTo(map);
