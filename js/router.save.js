62aa1a7e94381351a6d35bb25578e90d7b65a80e
Gabriel Landais <gabriel.landais@gmail.com>
2017-08-15 16:29:33 +0200

    save drawn items

--- js/router.save.js
+++ js/router.save.js
@@ -1,4 +1,20 @@
 L.easyButton('fa-save', function(btn, map) {
+	if (drawnItems.getLayers().length > 0) {
+		geojson = drawnItems.toGeoJSON();
+		gpxData = togpx(geojson);
+
+            var element = document.createElement('a');
+            element.setAttribute('href', 'data:application/gpx+xml;charset=utf-8,' + encodeURIComponent(gpxData));
+            element.setAttribute('download', 'export.gpx');
+
+            element.style.display = 'none';
+            document.body.appendChild(element);
+
+            element.click();
+
+            document.body.removeChild(element);
+		
+	}
 	if (map.__gpx) {
             var element = document.createElement('a');
             element.setAttribute('href', map.__gpx);
