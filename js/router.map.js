05e85c424fd7e05f6a34e9a592cce78f33109b2b
Gabriel Landais <gabriel.landais@gmail.com>
2017-08-15 16:11:44 +0200

    draw

--- js/router.map.js
+++ js/router.map.js
@@ -1,6 +1,7 @@
 var map = L.map('map', {
     zooms: [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21],
-	fullscreenControl: true
+	fullscreenControl: true,
+	drawControl: true
 });
 
 if (!map.restoreView()) {
