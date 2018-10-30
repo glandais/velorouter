41afa946e8332d2d86a1db7eacfc58ff2784e65d
Gabriel Landais <gabriel.landais@gmail.com>
2018-10-31 00:25:01 +0100

    Almost

--- js/router.save.js
+++ js/router.save.js
@@ -9,24 +9,17 @@ L.easyButton('fa-save', function (btn, map) {
     }
 	if (gpxData) {
         var element = document.createElement('a');
-        element.setAttribute('href', 'data:application/gpx+xml;charset=utf-8,' + encodeURIComponent(gpxData));
-        element.setAttribute('download', 'export.gpx');
-
+        element.href = 'data:application/gpx+xml;charset=utf-8,' + encodeURIComponent(gpxData);
+        element.download = 'export.gpx';
         element.style.display = 'none';
-        document.body.appendChild(element);
-
         element.click();
-
         document.body.removeChild(element);		
 	} else if (map.__gpx) {
         var element = document.createElement('a');
-        element.setAttribute('href', map.__gpx);
-
+        element.href = map.__gpx;
         element.style.display = 'none';
         document.body.appendChild(element);
-
         element.click();
-
         document.body.removeChild(element);
         return;
     }
@@ -47,8 +40,16 @@ L.easyButton('fa-image', function (btn, map) {
 		formData.append("tileUrl", "https://foil.fr/magic/magicCache/{z}/{x}/{y}.png");
 		var gpxFile = new Blob([gpxData], { type: "application/gpx+xml;charset=utf-8"});
 		formData.append("file", gpxFile);
-		
 		var request = new XMLHttpRequest();
+		request.responseType = 'blob';
+		request.onload = function (e) {
+			var blob = request.response;
+			var a = document.createElement("a");
+			a.style.display = 'none';
+			a.href = window.URL.createObjectURL(blob);
+			a.download = "export.png";
+			a.click();
+		}
 		request.open("POST", "https://gpx2web.appspot.com");
 		request.send(formData);
 	}
